aeadf05068139a2b8e16897bd3cdd14191e2adee
Fernando Costa <afjblt123@gmail.com>
2022-03-08 17:11:14 -0300

    Atualização do Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import Home from "./components/pages/Home";
