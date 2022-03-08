31230021fa0b4827a3ef56c0ec179f9e9a787d71
FernandoDev02 <dev02@rj-tecnologia.com>
2022-03-07 21:09:52 -0400

    Projeto em andamento

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import Home from "./components/pages/Home";
+import NewProject from "./components/pages/NewProject";
+
+
+import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Projects from "./components/pages/Projects";
+
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+        <Container customClass="min-height">
+          <Routes>
+              <Route exact path="/" element={<Home />}></Route>
+              <Route exact path="/company" element={<Company />}></Route>
+              <Route exact path="/contact" element={<Contact />}></Route>
+              <Route exact path="/projects" element={<Projects />}></Route>
+              <Route exact path="/newproject" element={<NewProject />}></Route>
+            
+          </Routes>
+        </Container>
+
+        <Footer />
+        
+     
+    </Router>
+  );
+}
+
+export default App;
