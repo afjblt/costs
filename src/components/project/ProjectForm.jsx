fe141a5a819165cbbd4d2c6f738af7f012214b9b
Fernando Costa <afjblt123@gmail.com>
2022-03-09 16:12:51 -0300

    Editando Rotas e configurando API/BD

--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -1,15 +1,53 @@
+import { useState, useEffect } from 'react';
+
 import Input from '../form/Input';
 import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
+
 import styles from './ProjectForm.module.css'
 
-function ProjectForm({ btnText }) {
+function ProjectForm({ btnText, handleSubmit, projectData }) {
+    const [categories, setCategories] = useState([])
+    const [project, setProject] = useState(projectData || {})
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            setCategories(data)
+        })
+        .catch(err => console.log(err))
+    }, [])
+
+    const submit = (e) => {
+        e.preventDefault()
+        handleSubmit(project)
+    }
+
+    function handleChange(e) {
+        setProject({ ...project, [e.target.name]: e.target.value })
+    }
+
+    function handleCategory(e) {
+        setProject({ ...project, category: {
+            id: e.target.value,
+            name: e.target.options[e.target.selectedIndex].text,
+        } })
+    }
+
+    
+
     return ( 
-        <form className={styles.form}>
-            <Input type='text' text='Nome do projeto' name='name' placeholder='Insira o nome do projeto'/>
-            <Input type='number' text='Orçamento do projeto' name='budget' placeholder='Insira o orçamento total'/>
+        <form onSubmit={submit} className={styles.form}>
+            <Input handleOnChange={handleChange} value={project.name ? project.name : ''} type='text' text='Nome do projeto' name='name' placeholder='Insira o nome do projeto'/>
+            <Input handleOnChange={handleChange} value={project.budget ? project.budget : ''} type='number' text='Orçamento do projeto' name='budget' placeholder='Insira o orçamento total'/>
             
-            <Select name='category_id' text='Selecione a categoria'/>
+            <Select handleOnChange={handleCategory} value={project.category ? project.category.id : ''} name='category_id' text='Selecione a categoria' options={categories}/>
 
             <SubmitButton text={btnText}/>
             
