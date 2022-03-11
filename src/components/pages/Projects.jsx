bf1d724540d5c5de75162ed38cac56af2c14edb1
Fernando Costa <afjblt123@gmail.com>
2022-03-11 14:50:57 -0300

    Colocando os projetos para serem vizualizados

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -2,16 +2,33 @@ import { useLocation } from "react-router-dom";
 import Message from "../layout/Message";
 import Container from '../layout/Container'
 import LinkButton from '../layout/LinkButton'
+import ProjectCard from "../project/ProjectCard";
 
 import styles from './Projects.module.css'
+import { useState, useEffect } from "react";
 
 function Projects() {
+    const [projects, setProjects] = useState([])
 
     const location = useLocation()
     let message = ''
     if (location.state) {
         message = location.state.message
     }
+
+    useEffect(() => {
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            setProjects(data)
+        })
+        .catch((err) => console.log(err))
+    }, [])
 
     return ( 
         <div className={styles.project_container}>
@@ -21,7 +38,11 @@ function Projects() {
             </div>
             {message && <Message type='success' msg={message} />}
             <Container customClass='start'>
-                <p>Projetos...</p>
+                {projects.length > 0 &&
+                    projects.map((project) => (
+                        <ProjectCard name={project.name} id={project.id} budget={project.budget} category={project.category.name} key={project.id}/>
+                    ))
+                }
             </Container>
             
         </div>
